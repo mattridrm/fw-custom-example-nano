34c964bf00d58748663bae5596460bece13accf8
mattridrm <mattridrm@gmail.com>
2025-07-11 23:45:26 -0400

    Update board.mk

--- board.mk
+++ board.mk
@@ -12,3 +12,8 @@ DDEFS += -DEFI_WIDEBAND_FIRMWARE_UPDATE=FALSE
 # assign critical LED to a non-existent pin if you do not have it on your board
 # good old PD14 is still the default value
 # DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
+
+# we do not have much Lua RAM, let's drop some fancy functions
+DDEFS += -DWITH_LUA_CONSUMPTION=FALSE
+DDEFS += -DWITH_LUA_PID=FALSE
+DDEFS += -DWITH_LUA_STOP_ENGINE=FALSE

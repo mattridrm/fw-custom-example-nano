b76928a544cf8ed2dfe84cfacd12ed72894e8d0b
GitHub git update Action <action@github.com>
2025-07-18 18:43:11 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_nano.h
+++ generated/controllers/generated/rusefi_generated_nano.h
@@ -1355,7 +1355,7 @@
 #define MAP_UPPER_LIMIT 650
 #define MAP_WINDOW_SIZE 8
 #define MAX_CYLINDER_COUNT 12
-#define MAX_TPS_PPS_DISCREPANCY 10
+#define MAX_TPS_PPS_DISCREPANCY 5
 #define mc33810maxDwellTimer_e_auto_enum 0="DWELL_2MS",3="DWELL_16MS",4="DWELL_32MS",1="DWELL_4MS",5="DWELL_64MS",2="DWELL_8MS"
 #define mc33810maxDwellTimer_e_DWELL_16MS 3
 #define mc33810maxDwellTimer_e_DWELL_2MS 0
@@ -1500,7 +1500,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 628914172
+#define SIGNATURE_HASH 1130088302
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2042,7 +2042,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.07.18.nano.628914172"
+#define TS_SIGNATURE "rusEFI main.2025.07.18.nano.1130088302"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_nano.h
+++ generated/controllers/generated/signature_nano.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 628914172
-#define TS_SIGNATURE "rusEFI main.2025.07.18.nano.628914172"
+#define SIGNATURE_HASH 1130088302
+#define TS_SIGNATURE "rusEFI main.2025.07.18.nano.1130088302"
